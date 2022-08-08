1ef90c5b625f5037691324964f764e08444e9a5f
jordelucas <jordevalucas@hotmail.com>
2022-08-08 18:04:44 -0300

    config lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components'
 
-import { defaultTheme } from './styles/themes/default';
-import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
